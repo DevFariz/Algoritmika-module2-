7ec950e6c42a92a9be56b4f3f2ae419b6433cd8d
R. Fariz <farizramazanov38@gmail.com>
2022-04-12 18:26:34 +0400

    feat: add popup for data input

---
+++ index.js
@@ -0,0 +1,6 @@
+const addBtn = document.querySelector(".add");
+const inpBlock = document.querySelector(".inp-block");
+
+addBtn.addEventListener("click", () => {
+    inpBlock.classList.add("active")
+})
