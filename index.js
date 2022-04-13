5c7a71db0e52702769f9a3c7d2d37721de973666
R. Fariz <farizramazanov38@gmail.com>
2022-04-13 18:34:36 +0400

    feat: add adding task

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const tasksBox = document.querySelector(".tasks");
+const input = document.querySelector(".inp");
+const text = document.querySelector(".task__content");
+const btnAdd = document.querySelector(".add");
+const btnRemove = document.querySelector(".remove")
 
 
-
+input.addEventListener("keyup", (event) => {
+  if(event.key == "Enter"){
+    text.style.padding = "12px 30px 12px 27px"
+    text.textContent = input.value;
+    input.remove()
+  }
+})
