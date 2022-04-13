b15c3324db6ea90311529657270f53c0a9dae6d0
R. Fariz <farizramazanov38@gmail.com>
2022-04-13 23:18:48 +0400

    feat: make remove button work

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const tasksBox = document.querySelector(".tasks");
 const input = document.querySelector(".inp");
 const text = document.querySelector(".task__content");
 const btnAdd = document.querySelector(".add");
-const btnRemove = document.querySelector(".remove");
 
+//adding element
 function addNewElement() {
   tasksBox.style.padding = "10px 0";
   const tempInp = document.createElement("li");
   tempInp.classList.add("task");
   tempInp.innerHTML = `<p class="task__content">${input.value}</p>
-        <button class="remove">
+        <button onclick="this.parentElement.remove()" class="remove">
             <svg class="remove-sv" width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
                 <path d="M6 6L14 14" stroke="#C4C4C4"/>
@@ -34,3 +34,10 @@ btnAdd.addEventListener("click", () => {
     input.style.display = "block";
   }
 });
+
+// removing element
+
+function removeElement(){
+
+}
+
